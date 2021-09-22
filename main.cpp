8c7e3b229da4f0a830121bef50063d109e57ef8b
pengc <pengchang@live.cn>
2021-09-22 11:41:42 +0800

    Health OK

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+#pragma comment(linker, "\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+int WinMain(HINSTANCE hInstance,
+            HINSTANCE hPrevInstance,
+            LPSTR lpCmdLine,
+            int nShowCmd) {
+    MessageBoxW(nullptr, L"请立刻活动颈椎以防止颈椎病!", L"健康提示", MB_OK);
+    return 0;
+}
