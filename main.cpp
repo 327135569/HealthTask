0de0c76ddd7f6c9359928813cfeed8de21486f45
pengc <pengchang@live.cn>
2021-09-22 13:45:23 +0800

    Health OK

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 int fakemain() {
-    MessageBoxW(nullptr, L"请立刻活动颈椎以防止颈椎病!", L"健康提示", MB_OK);
+    MessageBoxW(nullptr, L"请立刻活动颈椎以预防颈椎病!", L"健康提示", MB_ICONINFORMATION | MB_OK | MB_SYSTEMMODAL);
     return 0;
 }
